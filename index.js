bc6321e19a0d83a93cc2e3f09b153173ce9c0bdc
Harry Geoffrey Trebing <hgmtrebing@outlook.com>
2019-11-03 19:38:56 -0500

    Second commit

--- index.js
+++ index.js
@@ -5,19 +5,26 @@ const express = require("express");
 
 var app = express();
 
+// TODO - this sends an error message to the browser if there is an error - we must suppress this behavior
 app.get('/', function(req, res) {
-    res.send("hello world");
-})
+    res.sendFile("web/welcome.html", {root: __dirname});
+});
+
+app.get("/login", function(req, res) {
+    console.log("request received");
+    res.sendFile("web/login.html", {root: __dirname});
+});
+
+app.get("/create-account", function(req, res){
+    console.log("request received");
+    res.sendFile("web/new_user.html", {root: __dirname});
+});
 
 var server = https.createServer({
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert')
 }, app);
 
-app.listen(3000, function() {
-    console.log("LISTENING UNSECURE");
-});
-
 server.listen(8080, function() {
-    console.log("TEST");
+    console.log("HTTPS Server is now listening on port 8080");
 });
