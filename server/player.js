400e968c5bb57cd260b0fba057f0ba813023f326
Harry Geoffrey Trebing <hgmtrebing@outlook.com>
2019-11-17 14:49:09 -0500

    Additional Business logic - not finished yet

--- server/player.js
+++ server/player.js
@@ -1,11 +1,127 @@
 
-function Player (balance) {
-    this.balance = balance;
+function Player (seat, status) {
+    this.user = null;
+    this.seat = seat;
     this.status = "";
+    this.bets = 0;
     this.cardA = null;
     this.cardB = null;
 }
 
+function Status () {
+    this.EMPTY = "EMPTY";
+    this.ACTIVE = "ACTIVE";
+    this.FOLDED = "FOLDED";
+}
+
+function PlayerCollection () {
+    // EMPTY
+    // FOLDED
+    // ACTIVE
+
+    this.waitingUsers = [];
+    this.players = [
+        new Player(0, Status.EMPTY),
+        new Player(1, Status.EMPTY),
+        new Player(2, Status.EMPTY),
+        new Player(3, Status.EMPTY),
+        new Player(4, Status.EMPTY),
+        new Player(5, Status.EMPTY)
+    ];
+
+    this.setActive = function () {
+
+    };
+
+    this.addWaitingPlayers = function() {
+
+    };
+
+    // Use ALL to get total number of players
+    this.getNumberOfPlayers = function(status, matchesStatus) {
+        var playerNum = 0;
+        for (var i = 0; i < this.players.length; i++) {
+            if (matchesStatus && this.players[i].status === status){
+                playerNum++;
+            } else if (!matchesStatus && this.players[i].status !== status) {
+                playerNum++;
+            } else if (status === "ALL") {
+                playerNum++;
+            }
+        }
+        return playerNum;
+    };
+
+    // Use -1 as starting index if starting from the beginning of the array
+    this.getNextPlayerIndex = function(startingIndex, status, matchesStatus, inclusive, forwards) {
+
+        // Sanitize startingIndexes that are outside of the range of players
+        if (startingIndex < 0) {
+            startingIndex = 0;
+        } else if (startingIndex >= this.players.length) {
+            startingIndex = this.players.length-1;
+        }
+
+        var currentIndex;
+        var endingIndex;
+
+        // Logic to determine the initial value currentIndex - the counter variable
+        if (inclusive) {
+            currentIndex = startingIndex;
+        } else if (forwards && startingIndex >= this.players.length-1) {
+            currentIndex = 0;
+        } else if (forwards) {
+            currentIndex = startingIndex+1;
+        } else if (!forwards && startingIndex <= 0) {
+            currentIndex = this.players.length-1;
+        } else if (!forwards) {
+            currentIndex = startingIndex-1;
+        }
+
+        // Logic to determine the value of endingIndex - the index at which searching will stop
+        if (forwards && currentIndex === 0) {
+            endingIndex = this.players.length-1;
+        } else if (!forwards && currentIndex === this.players.length-1){
+            endingIndex = 0;
+        } else if (forwards) {
+            endingIndex = currentIndex - 1;
+        } else if (!forwards) {
+            endingIndex = currentIndex + 1;
+        }
+
+        // Keep iterating until current index reaches ending index
+        while (currentIndex !== endingIndex) {
+
+            if (matchesStatus && this.players[currentIndex].status === status) {
+                return currentIndex;
+            } else if (!matchesStatus && this.players[currentIndex].status !== status) {
+                return currentIndex;
+            }
+
+            if (forwards && currentIndex >= this.players.length-1) {
+                currentIndex = 0;
+            } else if (forwards){
+                currentIndex++;
+            } else if (!forwards && currentIndex <= 0) {
+                currentIndex = this.players.length-1;
+            } else if (!forwards) {
+                currentIndex--;
+            }
+        }
+        return null;
+    };
+
+    this.getPlayerAt = function(index) {
+        if (index >= this.players.length || index < 0) {
+            return null;
+        } else {
+            return this.players[index];
+        }
+    };
+}
+
 module.exports = {
-    Player: Player
+    Player: Player,
+    Status : Status,
+    PlayerCollection : PlayerCollection
 };
--- server/table.js
+++ server/table.js
@@ -2,43 +2,71 @@ var CardDeck = require('./carddeck').CardDeck;
 var Rank = require('./carddeck').Rank;
 var Suite = require('./carddeck').Suite;
 var Card = require('./carddeck').Card;
+var PlayerCollection = require('./player').PlayerCollection;
+var Status = require('./player').Status();
 var readline = require("readline");
 
 function Table(tableId) {
     this.tableId = tableId;
     this.roundId = 0;
+    this.tableActive = true;
+
     this.deck = new CardDeck();
-    this.players = [];
     this.flop = [];
     this.turn = null;
     this.river = null;
-    this.playersToAdd = [];
-    this.playersToRemove = [];
+
+    this.revealFlop = false;
+    this.revealTurn = false;
+    this.revealRiver = false;
+    this.revealOtherPlayers = false;
+
+    this.players = new PlayerCollection();
+
     this.pot = 0;
     this.currentBet = 0;
     this.bigBlindAmount = 0;
     this.smallBlindAmount = 0;
-    this.bigBlind = 0;
-    this.smallBlind = 0;
+    this.bigBlind = -1;
+    this.smallBlind = -1;
 
-    this.main = function() {
+    this.main = function () {
+        while (this.tableActive) {
+            this.waitForPlayers();
+            this.playGame();
+        }
+    };
 
+    this.waitForPlayers = function () {
+        this.players.addWaitingPlayers();
+        while (this.players.getNumberOfPlayers() < 2) {
+            this.this.players.addWaitingPlayers();
+        }
+    };
+
+    this.playGame = function () {
+        this.setupTable();
+        this.playBetRound();
+        this.playFlopRound();
+        this.playTurnRound();
+        this.playRiverRound();
+        this.determineWinner();
+        this.cleanupTable();
+    };
+
+    this.setupTable = function () {
         this.roundId++;
         this.deck.shuffle();
 
+        this.players.setActive();
+
         // Determine Blinds
-        this.bigBlind++;
-        this.smallBlind++;
-        if (this.smallBlind >= this.players.length) {
-            this.smallBlind = 0;
-        }
-        if (this.bigBlind >= this.players.length) {
-            this.bigBlind = 0;
-        }
+        this.bigBlind = this.players.getNextPlayerIndex(this.bigBlind, Status.ACTIVE, true);
+        this.smallBlind = this.players.getNextPlayerIndex(this.bigBlind, Status.ACTIVE, true);
 
         // Charge Blinds
-        this.players[this.smallBlind].balance -= this.smallBlindAmount;
-        this.players[this.bigBlind].balance -= this.bigBlindAmount;
+        this.players.getPlayerAt(this.smallBlind).balance -= this.smallBlindAmount;
+        this.players.getPlayerAt(this.bigBlind).balance -= this.bigBlindAmount;
         this.pot += this.smallBlindAmount;
         this.pot += this.bigBlindAmount;
 
@@ -46,59 +74,104 @@ function Table(tableId) {
         this.flop = this.deck.deck.splice(0, 3);
         this.turn = this.deck.deck.pop();
         this.river = this.deck.deck.pop();
-        for (var i = 0; i < this.players.length; i++) {
-            this.players[i].cardA = this.deck.deck.pop();
-            this.players[i].cardB = this.deck.deck.pop();
-        }
-
-        var lastPlayer = 0;
-
-    };
-
-    this.computeLastPlayer = function(currentPlayerIndex) {
-        if (currentPlayerIndex === 0) {
-            return this.players.length - 1;
-        } else {
-            return currentPlayerIndex-1;
+        for (var i = 0; i < this.players.getNumberOfPlayers(Status.ACTIVE, true); i++) {
+            this.players.getPlayerAt(i).cardA = this.deck.deck.pop();
+            this.players.getPlayerAt(i).cardB = this.deck.deck.pop();
         }
     };
-    
-    this.playRound = function () {
-        var currentPlayer = 0;
-        var lastPlayer = this.computeLastPlayer(currentPlayer);
+
+    this.playBetRound = function () {
+        if (this.players.getNumberOfPlayers(Status.ACTIVE, true) > 1) {
+            this.notifyPlayers();
+            this.conductBets();
+        }
+    };
+
+    this.playFlopRound = function () {
+        this.revealFlop = true;
+        if (this.players.getNumberOfPlayers(Status.ACTIVE, true) > 1) {
+            this.notifyPlayers();
+            this.conductBets();
+        }
+    };
+
+    this.playTurnRound = function () {
+        this.revealTurn = true;
+        if (this.players.getNumberOfPlayers(Status.ACTIVE, true) > 1) {
+            this.notifyPlayers();
+            this.conductBets();
+        }
+    };
+
+    this.playRiverRound = function () {
+        this.revealRiver = true;
+        if (this.players.getNumberOfPlayers(Status.ACTIVE, true) > 1) {
+            this.notifyPlayers();
+            this.conductBets();
+        }
+    };
+
+    this.determineWinner = function () {
+        this.revealOtherPlayers = true;
+
+        this.notifyPlayers();
+    };
+
+    this.cleanupTable = function () {
+        // Remove cards and place them back in the deck
+        this.deck.deck.push(this.flop.pop());
+        this.deck.deck.push(this.flop.pop());
+        this.deck.deck.push(this.flop.pop());
+        this.deck.deck.push(this.turn);
+        this.deck.deck.push(this.river);
+        this.turn = null;
+        this.river = null;
+
+        // Set all reveal variables to false
+        this.revealFlop = false;
+        this.revealTurn = false;
+        this.revealRiver = false;
+        this.revealOtherPlayers = false;
+
+        // Reset Bets and Pots
+        this.pot = 0;
+        this.currentBet = 0;
+
+        // Add Player Hands back
+        for (var i = 0; i < this.players.getNumberOfPlayers(); i++) {
+            this.deck.deck.push(this.players.getPlayerAt(i).handA);
+            this.deck.deck.push(this.players.getPlayerAt(i).handB);
+        }
+    };
+
+    this.conductBets = function () {
+
+        var currentPlayer = this.players.getNextPlayerIndex(0, Status.ACTIVE, true, true, true);
+        var lastPlayer = this.players.getNextPlayerIndex(0, Status.ACTIVE, true, false, false);
 
         while (currentPlayer !== lastPlayer) {
-            var action = this.receiveUserAction();
+            var input = this.getUserInput();
 
-            if (action.name === "CHECK") {
+            if (input.name === "CHECK") {
 
-            } else if (action.name === "FOLD") {
-                this.players[currentPlayer].status = "FOLDED";
-            } else if (action.name === "LEAVE") {
+            } else if (input.name === "FOLD") {
+                this.players.getPlayerAt(currentPlayer).status = Status.FOLDED;
+            } else if (input.name === "LEAVE") {
 
-            } else if (action.name === "CALL") {
+            } else if (input.name === "CALL") {
 
-            } else if (action.name === "RAISE") {
+            } else if (input.name === "RAISE") {
+                lastPlayer = currentPlayer;
+
+            } else if (input.name === "TIMEOUT") {
 
             } else {
 
             }
-
-            currentPlayer++;
         }
-    }
-
-    this.receivePlayerAction = function () {
-        this.testReceive();
-    }
-
-    this.testReceive = function() {
-
-    }
+    };
 }
 
 module.exports = {
     Table : Table
 };
-
-
